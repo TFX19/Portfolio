284bb33e36252210faf064d9611d99aca3f649e0
Tiago Borges <103573703+TFX19@users.noreply.github.com>
2025-03-09 21:02:10 +0000

    removed an import

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import Link from "next/link";
-import MotionWrapper from "../components/client/MotionWrapper";
 export const metadata = {
   title: "About - Tiago Borges"
 };
