e5ac57157bf948c4a0ae7265dbd7714523de42d3
Tiago Borges <103573703+TFX19@users.noreply.github.com>
2025-03-11 17:53:55 +0000

    added onscroll animation on projectpage

--- app/work/ProjectPage.tsx
+++ app/work/ProjectPage.tsx
@@ -3,12 +3,13 @@ import Carousel from "@/app/components/ui/Caroussel";
 import { Globe, Code, Languages, Info } from "lucide-react";
 import Link from "next/link";
 import ShowList from "@/app/components/client/ShowList";
+import ScrollAnimation from "../components/client/OnScrollAnim";
 
 interface ProjectPageProps {
   title: string;
   description: string;
   descriptiontf: string;
-  descriptionpe:string;
+  descriptionpe: string;
   moreInfo?: string;
   images: {
     imageUrl1: string;
@@ -39,7 +40,9 @@ const ProjectPage: React.FC<ProjectPageProps> = ({
   return (
     <div className="p-5 py-10 lg:px-40 lg:py-20 bg-base-200 min-h-screen">
       <div className="items-center flex-col lg:flex-row">
-        <h1 className="text-xl mb-5">Project: <b>{title}</b></h1>
+        <h1 className="text-xl mb-5">
+          Project: <b>{title}</b>
+        </h1>
         <Carousel
           imageUrl1={images.imageUrl1}
           title1={images.title1}
@@ -50,51 +53,74 @@ const ProjectPage: React.FC<ProjectPageProps> = ({
           imageUrl4={images.imageUrl4}
           title4={images.title4}
         />
-        <h2 className="text-xl font-bold mt-10">Project Objective</h2>
-        <p className="mt-5 lg:text-lg">{description}</p>
+        <ScrollAnimation>
+          <h2 className="text-xl font-bold mt-10">Project Objective</h2>
+          <p className="mt-5 lg:text-lg">{description}</p>
+        </ScrollAnimation>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 p-10">
-        <div className="space-y-4">
-          <h2 className="text-lg font-bold">Technologies</h2>
-
-          <ShowList technologies={technologies} />
-        </div>
-        <div className="col-span-1 lg:col-span-1">
-          <div className="flex items-center">
-            <Globe className="mr-2" />
-            {link ? (
-              <Link href={link} target="_blank" rel="noopener noreferrer">
-                <span className="text-base text-green-500">Online</span>
+        <ScrollAnimation>
+          <div className="space-y-4">
+            <h2 className="text-lg font-bold">Technologies</h2>
+            <ShowList technologies={technologies} />
+          </div>
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <div className="col-span-1 lg:col-span-1">
+            <div className="flex items-center">
+              <Globe className="mr-2" />
+              {link ? (
+                <Link href={link} target="_blank" rel="noopener noreferrer">
+                  <span className="text-base text-green-500 underline">Online</span>
+                </Link>
+              ) : (
+                <span className="text-base text-red-500">Offline</span>
+              )}
+            </div>
+            <div className="flex items-center mt-5">
+              <Code className="mr-2" />
+              <Link
+                className="underline underline-offset-4 text-indigo-500"
+                href={code}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Code
               </Link>
-            ) : (
-              <span className="text-base text-red-500">Offline</span>
-            )}
-          </div> 
-          <div className="flex items-center mt-5">
-            <Code className="mr-2" />
-            <Link className="underline underline-offset-4 text-indigo-500" href={code} target="_blank" rel="noopener noreferrer">
-              Code
-            </Link>
+            </div>
+            <div className="flex items-center mt-5">
+              <Languages className="mr-2" />
+              <span>Portuguese</span>
+            </div>
+            <div className="flex items-center mt-5">
+              {moreInfo && (
+                <div className="flex items-center">
+                  <Info className="mr-2" />
+                  <Link
+                    href={moreInfo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="underline underline-offset-4 text-indigo-500"
+                  >
+                    <span>More about</span>
+                  </Link>
+                </div>
+              )}
+            </div>
           </div>
-          <div className="flex items-center mt-5">
-            <Languages className="mr-2" />
-           <span>Portuguese</span>
-          </div>
-          <div className="flex items-center mt-5">
-          {moreInfo && (
-              <div className="flex items-center">
-                <Info className="mr-2" />
-                <Link href={moreInfo} target="_blank" rel="noopener noreferrer" className="underline underline-offset-4 text-indigo-500"><span>More about</span></Link>
-              </div>
-            )}
-          </div>
-        </div>
+        </ScrollAnimation>
       </div>
       <div className="items-center flex-col lg:flex-row">
-        <h2 className="text-xl font-bold mt-10">Tech Stack & Functionalities</h2>
-        <p className="mt-5 lg:text-lg break-normal">{descriptiontf}</p>
-        <h2 className="text-xl font-bold mt-10">Potential Enhancements</h2>
-        <p className="mt-5 lg:text-lg">{descriptionpe}</p>
+        <ScrollAnimation>
+          <h2 className="text-xl font-bold mt-10">
+            Tech Stack & Functionalities
+          </h2>
+          <p className="mt-5 lg:text-lg break-normal">{descriptiontf}</p>
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <h2 className="text-xl font-bold mt-10">Potential Enhancements</h2>
+          <p className="mt-5 lg:text-lg">{descriptionpe}</p>
+        </ScrollAnimation>
       </div>
     </div>
   );
